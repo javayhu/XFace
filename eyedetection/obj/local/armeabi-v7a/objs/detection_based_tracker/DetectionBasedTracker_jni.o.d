df0014e2d13f04ec508cb311dedc515d8add8a9f
yinger090807 <1158112684@qq.com>
2013-10-30 15:25:35 +0800

    hujiawei opencv demo - eye detection from http://romanhosek.cz/android-eye-detection-and-tracking-with-opencv/

---
+++ eyedetection/obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o.d
@@ -0,0 +1,145 @@
+obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o: \
+ jni/DetectionBasedTracker_jni.cpp jni/DetectionBasedTracker_jni.h \
+ /Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/jni.h \
+ /Users/hujiawei/Android/opencv_sdk/sdk/native/jni/include/opencv2/core/core.hpp \
+ /Users/hujiawei/Android/opencv_sdk/sdk/native/jni/include/opencv2/core/types_c.h \
+ /Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/assert.h \
+ /Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ /Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ /Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/stdlib.h \
+ /Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/string.h \
+ /Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/malloc.h \
+ /Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/alloca.h \
+ /Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/strings.h \
+ /Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ /Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/stdint.h \
+ /Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ /Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ /Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ /Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ /Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ /Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ /Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ /Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ /Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ /Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/memory.h \
+ /Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/math.h \
+ /Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/limits.h \
+ /Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ /Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ /Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ /Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/machine/limits.h \
+ /Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/sys/syslimits.h \
+ /Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/asm/page.h \
+ /Users/hujiawei/Android/opencv_sdk/sdk/native/jni/include/opencv2/core/version.hpp \
+ /Users/hujiawei/Android/android_ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm \
+ /Users/hujiawei/Android/android_ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath \
+ /Users/hujiawei/Android/android_ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef \
+ /Users/hujiawei/Android/android_ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/complex \
+ /Users/hujiawei/Android/android_ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/map \
+ /Users/hujiawei/Android/android_ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/vector \
+ /Users/hujiawei/Android/opencv_sdk/sdk/native/jni/include/opencv2/core/operations.hpp \
+ /Users/hujiawei/Android/android_ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/limits \
+ /Users/hujiawei/Android/opencv_sdk/sdk/native/jni/include/opencv2/core/mat.hpp \
+ /Users/hujiawei/Android/opencv_sdk/sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp \
+ /Users/hujiawei/Android/opencv_sdk/sdk/native/jni/include/opencv2/objdetect/objdetect.hpp \
+ /Users/hujiawei/Android/android_ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/deque \
+ /Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/android/log.h
+
+jni/DetectionBasedTracker_jni.h:
+
+/Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/jni.h:
+
+/Users/hujiawei/Android/opencv_sdk/sdk/native/jni/include/opencv2/core/core.hpp:
+
+/Users/hujiawei/Android/opencv_sdk/sdk/native/jni/include/opencv2/core/types_c.h:
+
+/Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/assert.h:
+
+/Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+/Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/stdlib.h:
+
+/Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/string.h:
+
+/Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+/Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/alloca.h:
+
+/Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/strings.h:
+
+/Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+/Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+/Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+/Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+/Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+/Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+/Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+/Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+/Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+/Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/memory.h:
+
+/Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/math.h:
+
+/Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/limits.h:
+
+/Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+
+/Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+
+/Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+
+/Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/machine/limits.h:
+
+/Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
+
+/Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/asm/page.h:
+
+/Users/hujiawei/Android/opencv_sdk/sdk/native/jni/include/opencv2/core/version.hpp:
+
+/Users/hujiawei/Android/android_ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm:
+
+/Users/hujiawei/Android/android_ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath:
+
+/Users/hujiawei/Android/android_ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef:
+
+/Users/hujiawei/Android/android_ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/complex:
+
+/Users/hujiawei/Android/android_ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/map:
+
+/Users/hujiawei/Android/android_ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/vector:
+
+/Users/hujiawei/Android/opencv_sdk/sdk/native/jni/include/opencv2/core/operations.hpp:
+
+/Users/hujiawei/Android/android_ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/limits:
+
+/Users/hujiawei/Android/opencv_sdk/sdk/native/jni/include/opencv2/core/mat.hpp:
+
+/Users/hujiawei/Android/opencv_sdk/sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp:
+
+/Users/hujiawei/Android/opencv_sdk/sdk/native/jni/include/opencv2/objdetect/objdetect.hpp:
+
+/Users/hujiawei/Android/android_ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/deque:
+
+/Users/hujiawei/Android/android_ndk/platforms/android-8/arch-arm/usr/include/android/log.h:
