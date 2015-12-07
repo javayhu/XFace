bab090843a95e15629a311ae663baec47ac199c1
潇涧 <hujiawei090807@gmail.com>
2015-12-07 23:23:51 +0800

    add screenshot

--- AndroidStudio/XFace/OpenCVLibrary244/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
+++ AndroidStudio/XFace/OpenCVLibrary244/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /Users/hujiawei/AndroidStudioProjects/XFace/OpenCVLibrary244/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
+ * Original file: /Users/hujiawei/Github/XFace/AndroidStudio/XFace/OpenCVLibrary244/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
